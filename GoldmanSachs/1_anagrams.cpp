ab2885572c5b7f927aca630b39608dbfbeccfce1
Thebitbotcoder <77109162+thebitbotcoder@users.noreply.github.com>
2022-01-02 22:21:04 +0530

    Create 1_anagrams.cpp

---
+++ GoldmanSachs/1_anagrams.cpp
@@ -0,0 +1,56 @@
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        map<string, vector<string>> mymap;
+        for(auto x: string_list){
+            string s1 = x;
+            sort(x.begin(), x.end());
+            mymap[x].push_back(s1);
+        }
+        vector<vector<string> > result;
+        for (auto i : mymap){
+            result.push_back(i.second);
+        }
+        return result;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
